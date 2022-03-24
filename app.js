31f697c52b930416b844954a5546539a655dc46d
Fandy Barestu <fandy@softwareseni.com>
2022-03-24 17:02:12 +0700

    Add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
